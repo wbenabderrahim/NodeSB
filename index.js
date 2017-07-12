2aad82116f6937c847c573f9efbb74f7affe358f
wbenabderrahim <bmehri@skillsfactory.eu>
2017-07-11 17:00:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use('/', router)
  app.listen("3000",function(){
     console.log('running on port 3000'); 
    setTimeout(function () {
-        }, 113000)
+        }, 13000)
 });
 module.exports = app ;
