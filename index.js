8eb05432c31abe9e71c49a4ec5b506a592f624df
wbenabderrahim <bmehri@skillsfactory.eu>
2017-07-12 11:25:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ app.set('view engine','ejs');
 app.set('views',__dirname+'/views');
 app.use('/', router)
 
- app.listen("3000",function(){
+ var server = app.listen("3000",function(){
     console.log('running now on port 3000'); 
 }); 
 
 module.exports = app ;
+module.exports.fun =  function () { 
+    server.close() ;
+};
