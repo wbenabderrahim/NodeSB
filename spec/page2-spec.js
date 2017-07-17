50c81824c5f4cd8957d7d4de92940ceddb3fd124
wbenabderrahim <bmehri@skillsfactory.eu>
2017-07-17 16:07:55 +0100

    Update page2-spec.js

--- spec/page2-spec.js
+++ spec/page2-spec.js
@@ -18,7 +18,7 @@ describe("test3", function() {
         var text1 = $page1("body").text();
          text1 = text1.replace(/\s+/g, "")
           expect(text1).toBe("Second");
-   setTimeout(helloWorld.fun, 3000); 
+   setTimeout( helloWorld.fun, 4000); 
         done();
       });
     });
